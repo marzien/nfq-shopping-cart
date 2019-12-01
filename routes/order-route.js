42a1fb56ad7273d2ad08e3ec020d3ab13cdfa9d3
Marius Zienius <marius.zienius@gmail.com>
2019-12-01 22:00:50 +0100

    back create order functionality

--- routes/order-route.js
+++ routes/order-route.js
@@ -35,4 +35,57 @@ router.get('/productsByOrders', (req, res, next) => {
 		})
 })
 
+router.post('/order', function(req, res){
+    let productID = req.body.product;
+    let orderQuant = req.body.quantity;
+    let userID = req.body.user;
+
+    Promise.all([
+        User.getUserMoney(userID), //give back userMoney
+        Product.getProductData(productID) //give back productQuant productPrice
+    ]).then(values => {
+        let userMoney = values[0];
+        let productQuant = values[1][0];
+        let productPrice = values[1][1];
+        console.log('User money: ', userMoney);
+        console.log('Product quant in shop: ', productQuant);
+        console.log('Product price: ', productPrice);
+        console.log('Order quantity ', orderQuant);
+        // checking conditions
+        if (orderQuant > productQuant) {
+            console.log('Not enough product quantity in shop');
+        } else if (userMoney < productPrice * orderQuant) {
+            console.log('Not enough users money for purchase');
+        } else {
+            // create Order record
+            Order.createOrder(userID, productID, orderQuant, function(err, user){
+                if(err) {
+                    console.log('Error ' + err.message);
+                }
+                console.log('Order created!');
+            });
+            // deduct money from user
+            let newUserMoney = userMoney-(productPrice * orderQuant);
+            User.deductUserMoney(userID, newUserMoney, {new:true}, function(err, user){
+                // {new:true} - printout new value
+                if(err){
+                    console.log('Error ' + err.message);
+                }
+                res.json(user);
+                console.log('User money updated!');
+            });
+
+            // update Product quantity
+            let newProductQuant = productQuant - orderQuant;
+            Product.updateProductQuantity(productID, newProductQuant, {new:true}, function(err, product){
+                if(err){
+                    console.log('Error ' + err.message);
+                }
+                //res.json(product);
+                console.log('Product quantity updated!');
+            });
+        }
+    })
+});
+
 module.exports = router
